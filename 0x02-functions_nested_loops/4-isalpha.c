0f8086ff861572ce7887e320b5a23ecbbef90b27
Deric049 <drckmasiga@gmail.com>
2022-07-05 14:03:03 -0700

    task4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _isalpha - check for aphabetic character
+ *@c: input character
+ *
+ * Return: 1 (Success) 0 (Otherwise)
+ */
+int _isalpha(int c)
+{
+	int alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		if (c == alpha)
+			return (1);
+	}
+
+	for (alpha = 'A'; alpha <= 'Z'; alpha++)
+	{
+		if (c == alpha)
+			return (1);
+	}
+	return (0);
+}

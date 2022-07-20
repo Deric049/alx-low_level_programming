3977656cb9740d1c8471a5432709d7f8afe8431e
Deric049 <107037684+Deric049@users.noreply.github.com>
2022-07-20 09:43:01 +0300

    done

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,24 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+int _putchar(char c);
+
+/**
+ * _puts_recursion - print a string using recursion
+ * @s: String to print
+ *
+ * Return: void
+ */
+
+void _puts_recursion(char *s);
+
+/**
+ * _print_rev_recursion - print a string in rev using recursion
+ * @s: String to print
+ *
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s);
+
+#endif
